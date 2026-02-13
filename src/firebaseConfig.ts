18c9a6e040b30853bcbe880a9435cda248e5b2a0
amitkumarpc7 <amitkpc11@gmail.com>
2026-02-13 22:58:08 +0530

    updated firebaseConfig

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -3,18 +3,17 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBjnRjryuwgPyvA7ZE2SpVtK9mVMBVRHpM",
-  authDomain: "acslab-5293f.firebaseapp.com",
-  projectId: "acslab-5293f",
-  storageBucket: "acslab-5293f.firebasestorage.app",
-  messagingSenderId: "931501820560",
-  appId: "1:931501820560:web:ae22536ed557e03093a615",
-  measurementId: "G-VX8W2LP802",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Export services to use in your Admin and Catalog pages
 export const db = getFirestore(app);
 export const storage = getStorage(app);
